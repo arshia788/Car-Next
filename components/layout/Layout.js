ff8025f557e4a332ba2a84bedb39132c06dc47c9
Amirali Amanipour <arshia.ala80@gmail.com>
2023-06-28 11:19:57 +0330

    worked on layout nav ,, footer

---
+++ components/layout/Layout.js
@@ -0,0 +1,34 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Layout = ({ children }) => {
+
+    return (
+        <>
+            <header className='flex justify-center items-center text-center '>
+                <Link href="/"
+                    className='bg-green-600 w-60 p-2 m-1 rounded-sm'
+                >
+                    <h2 className='text-2xl text-teal-50
+                    mb-1 border-b-2 
+                    '>BOTOCAR</h2>
+                    <p className='text-white
+                    
+                    '>choose and buy a car</p>
+                </Link>
+            </header>
+
+            <div className='w-1/2 my-8 mx-auto h-screen'>
+                {children}
+            </div>
+
+            <footer className='text-white bg-green-600
+            p-2 text-center
+            '>
+                This Project Is By Next.js &copy;
+            </footer>
+        </>
+    );
+};
+
+export default Layout;
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,11 @@
 import '../styles/globals.css';
-import Layout from './layout';
-
+import Layout from '../components/layout/Layout';
 
 function MyApp({ Component, pageProps }) {
-  
+
   return (
-    <Layout>
-      <Component {...pageProps}/>
+    <Layout a="milad" >
+      <Component {...pageProps} />
     </Layout>
   )
 }
