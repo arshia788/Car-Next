8a42829361d8c7def1286be7220e6bc8f1ae5505
Amirali Amanipour <arshia.ala80@gmail.com>
2023-06-28 14:37:38 +0330

    routes categories

--- components/module/Categories.js
+++ components/module/Categories.js
@@ -8,33 +8,45 @@ const Categories = () => {
     return (
         <div className="flex justify-between 
          items-center w-10/12 mx-auto gap-x-1">
-            <div className="flex flex-col
+
+            <Link href='/categories/sedan' >
+                <div className="flex flex-col
              bg-green-600 
+             py-1 px-8 rounded-sm text-lg
+             text-white items-center">
+                    <p>sedan</p>
+                    <Sedan
+                    />
+                </div>
+            </Link>
+
+            <Link href='/categories/suv'>
+                <div className="flex flex-col bg-green-600 
             py-1 px-8 rounded-sm text-lg
             text-white items-center">
-                <p>sedan</p>
-                <Sedan 
-                // className='ml-'
-                />
-            </div>
-            <div className="flex flex-col bg-green-600 
-            py-1 px-8 rounded-sm text-lg
-            text-white items-center">
-                <p>suv</p>
-                <Suv />
-            </div>
-            <div className="flex flex-col bg-green-600 
+                    <p>suv</p>
+                    <Suv />
+                </div>
+            </Link>
+
+
+            <Link href='/categories/hatchback'>
+                <div className="flex flex-col bg-green-600 
             py-1 px-6 rounded-sm text-lg
             text-white items-center">
-                <p>Hatchback</p>
-                <Hatchback />
-            </div>
-            <div className="flex flex-col bg-green-600 
+                    <p>Hatchback</p>
+                    <Hatchback />
+                </div>
+            </Link>
+
+            <Link href='/categories/sport'>
+                <div className="flex flex-col bg-green-600 
             py-1 px-8 rounded-sm text-lg
             text-white items-center">
-                <p>sport</p>
-                <Sport />
-            </div>
+                    <p>sport</p>
+                    <Sport />
+                </div>
+            </Link>
 
         </div>
     );
---
+++ pages/categories/sedan.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const sedan = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default sedan;
